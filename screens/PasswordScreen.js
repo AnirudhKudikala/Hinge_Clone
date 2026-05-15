a89a2f6bf7bd5a963fbc0d1161edaf3afdec2aec
AnirudhKudikala <anirudhkudikala01@gmail.com>
2026-05-16 01:05:45 +0530

    Password Screen UI

--- screens/PasswordScreen.js
+++ screens/PasswordScreen.js
@@ -1,14 +1,104 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useState } from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Ionicons from '@react-native-vector-icons/ionicons';
+import MaterialDesignIcons from '@react-native-vector-icons/material-icons';
 
 const PasswordScreen = () => {
+  const [password, setPassword] = useState('');
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const email = route.params.email;
+
+  const onChangePassword = enteredPassword => {
+    setPassword(enteredPassword);
+  };
+
+  const handleNext = () => {
+    navigation.navigate('Otp', {email});
+  };
+
   return (
-    <View>
-      <Text>PasswordScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={{ marginTop: 80, marginHorizontal: 20 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <View
+            style={{
+              width: 44,
+              height: 44,
+              borderRadius: 22,
+              borderWidth: 2,
+              borderColor: 'black',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <MaterialDesignIcons name="lock" size={26} color="black" />
+          </View>
+          <Image
+            style={{ width: 100, height: 40 }}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}
+          />
+        </View>
+        <Text
+          style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-bold',
+            marginTop: 15
+          }}
+        >
+          Please choose a password
+        </Text>
 
-export default PasswordScreen
+        <TextInput
+          value={password}
+          onChange={onChangePassword}
+          autoFocus
+          secureTextEntry
+          placeholder="Enter your password"
+          placeholderTextColor={'#BEBEBE'}
+          style={{
+            width: 340,
+            marginVertical: 10,
+            marginTop: 25,
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            paddingBottom: 10,
+            fontFamily: 'GeezaPro-Bold',
+            fontSize: 22,
+          }}
+        />
+        <Text style={{ color: 'grey', marginTop: 7, fontSize: 15 }}>
+          Note: Your details will be safe with us
+        </Text>
+        <TouchableOpacity
+          onPress={handleNext}
+          activeOpacity={0.8}
+          style={{ marginTop: 30, marginLeft: 'auto' }}
+        >
+          <Ionicons
+            name="chevron-forward-circle-outline"
+            size={45}
+            color="#581845"
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default PasswordScreen;
+
+const styles = StyleSheet.create({});

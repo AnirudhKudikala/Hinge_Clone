0aee4665a3367062dc21f64425fef26ea681cd8a
AnirudhKudikala <anirudhkudikala01@gmail.com>
2026-05-17 00:10:23 +0530

    Home Town screen UI

--- screens/HomeTownScreen.js
+++ screens/HomeTownScreen.js
@@ -1,14 +1,98 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Ionicons from '@react-native-vector-icons/ionicons';
+import FontAwesome from '@react-native-vector-icons/fontawesome';
+import MaterialDesignIcons from '@react-native-vector-icons/material-design-icons';
+import { useNavigation } from '@react-navigation/native';
 
 const HomeTownScreen = () => {
+  const [hometown, setHomeTown] = useState('');
+  const navigation = useNavigation();
+
+  const handleNext = () => {
+    navigation.navigate('WorkPlace');
+  };
   return (
-    <View>
-      <Text>HomeTownScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={{ marginTop: 80, marginHorizontal: 20 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <View
+            style={{
+              width: 44,
+              height: 44,
+              borderRadius: 22,
+              borderWidth: 2,
+              borderColor: 'black',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <MaterialDesignIcons
+              name="location-enter"
+              size={23}
+              color="black"
+            />
+          </View>
+          <Image
+            style={{ width: 100, height: 40 }}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}
+          />
+        </View>
 
-export default HomeTownScreen
+        <Text
+          style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginTop: 15,
+          }}
+        >
+          Where's your hometown?
+        </Text>
 
-const styles = StyleSheet.create({})
+        <TextInput
+          autoFocus={true}
+          value={hometown}
+          onChangeText={text => setHomeTown(text)}
+          placeholder="HomeTown"
+          style={{
+            width: 340,
+            marginTop: 25,
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            paddingBottom: 10,
+            fontFamily: 'GeezaPro-Bold',
+            fontSize: hometown ? 22 : 22,
+          }}
+        />
+
+        <TouchableOpacity
+          onPress={handleNext}
+          activeOpacity={0.8}
+          style={{ marginTop: 30, marginLeft: 'auto' }}
+        >
+          <Ionicons
+            name="chevron-forward-circle-outline"
+            size={45}
+            color="#581845"
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeTownScreen;
+
+const styles = StyleSheet.create({});

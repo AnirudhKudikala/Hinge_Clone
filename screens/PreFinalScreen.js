a4565e0c796e7e49cf6882a867d153e570a271d1
AnirudhKudikala <anirudhkudikala01@gmail.com>
2026-05-17 00:45:09 +0530

    Pre final screen UI

--- screens/PreFinalScreen.js
+++ screens/PreFinalScreen.js
@@ -1,14 +1,85 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  ActivityIndicator,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import LottieView from 'lottie-react-native';
+import {useNavigation} from '@react-navigation/native';
 
 const PreFinalScreen = () => {
+  const [userData, setUserData] = useState();
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
+
+  const registerUser = () => {
+  };
   return (
-    <View>
-      <Text>PreFinalScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={{marginTop: 80}}>
+        <Text
+          style={{
+            fontSize: 32,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginLeft: 20,
+          }}>
+          All set to register.
+        </Text>
 
-export default PreFinalScreen
+        <Text
+          style={{
+            fontSize: 32,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginLeft: 20,
+            marginTop: 10,
+          }}>
+          Setting up your profile for you.
+        </Text>
+      </View>
 
-const styles = StyleSheet.create({})
+      <View>
+        <LottieView
+          style={{
+            height: 260,
+            width: 300,
+            alignSelf: 'center',
+            marginTop: 40,
+            justifyContent: 'center',
+          }}
+          source={require('../assets/love.json')}
+          autoPlay
+          loop={true}
+          speed={0.7}
+        />
+      </View>
+
+      <Pressable
+        onPress={registerUser}
+        style={{marginTop: 'auto', backgroundColor: '#900C3F', padding: 15}}>
+        {loading ? (
+          <ActivityIndicator size="small" color="white" />
+        ) : (
+          <Text
+            style={{
+              textAlign: 'center',
+              color: 'white',
+              fontWeight: '600',
+              fontSize: 15,
+            }}>
+            Finish Registering
+          </Text>
+        )}
+      </Pressable>
+    </SafeAreaView>
+  );
+};
+
+export default PreFinalScreen;
+
+const styles = StyleSheet.create({});
